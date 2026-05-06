4bd4b53926605eeb6f4af8c9bdc29242bfba403a
NDAM TAYOU DANIEL ANDY MAEL <danielandymael@gmail.com>
2026-05-06 10:53:02 +0000

    optimisation avec beforeEach

--- src/bookstoread/BookShelfSpec.java
+++ src/bookstoread/BookShelfSpec.java
@@ -2,11 +2,18 @@ package bookstoread;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BookShelfSpec {
+    private BookShelf shelf;
+
+    @BeforeEach
+    void init() throws Exception {
+        shelf = new BookShelf();
+    }
     @Test
     public void shelfEmptyWhenNoBookAdded() throws Exception {
         BookShelf shelf = new BookShelf();
