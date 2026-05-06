a5114fb6331170e7107ccb730b2836ee3c8d801c
NDAM TAYOU DANIEL ANDY MAEL <danielandymael@gmail.com>
2026-05-06 11:52:07 +0000

    modification des string par les objet books

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -1,20 +1,34 @@
 package bookstoread;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class BookShelf {
-    private final List<String> books = new ArrayList<>();
-    public List<String> books() {
+    private final List<Book> books = new ArrayList<>();
+
+    public List<Book> books() {
         return Collections.unmodifiableList(books);
     }
 
-    public void add(String... booksToAdd) {
-        Arrays.stream(booksToAdd).forEach(book ->
-                books.add(book));
+    public void add(Book... booksToAdd) {
+        books.addAll(Arrays.asList(booksToAdd));
     }
 
-    public List<String> arrange() {
-        return books.stream().sorted().collect(Collectors.toList());
+    public List<Book> arrangeByTitle() {
+        return books.stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
 
+    public List<Book> arrangeByAuthor() {
+        return books.stream()
+                .sorted(Comparator.comparing(Book::getAuthor))
+                .collect(Collectors.toList());
+    }
+
+    public List<Book> arrangeByPublicationDate() {
+        return books.stream()
+                .sorted(Comparator.comparing(Book::getPublishedOn))
+                .collect(Collectors.toList());
     }
 }
